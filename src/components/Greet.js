0adcd62218acb86241e0cfaccf4dd2c0bbe9582a
Durgesh Pratap Singh <durgesh120103@gmail.com>
2025-02-10 20:41:05 +0530

    destructuring props and state in class and function components

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -6,13 +6,35 @@ import React from 'react';
 // }
 
 //arrowfunction
+// const Greet = (props) =>{
+//     // console.log(props);
+//     return(
+//         <div>
+//             <h1>Hello {props.name} has height {props.height} </h1>
+//             {props.children}
+//         </div>
+//     )
+// } 
+
+// destructuring props in the function parameter
+// const Greet = ({name,height,children}) =>{
+//     // console.log(props);
+//     return(
+//         <div>
+//             <h1>Hello {name} has height {height} </h1>
+//             {children}
+//         </div>
+//     )
+// } 
+
+// destructuring props in the function body
 const Greet = (props) =>{
     // console.log(props);
-    
+    const {name,height,children} = props
     return(
         <div>
-            <h1>Hello {props.name} has height {props.height} </h1>
-            {props.children}
+            <h1>Hello {name} has height {height} </h1>
+            {children}
         </div>
     )
 } 
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,8 +1,20 @@
 import React,{Component} from 'react';
 
+// class Welcome extends Component{
+//     render(){
+//         return <h1>Class Component of {this.props.topic} and {this.props.subtopic}</h1>
+//         // console.log(this.props);
+        
+//     }
+// }
+
+// destructuring props in the class body
 class Welcome extends Component{
+    
     render(){
-        return <h1>Class Component of {this.props.topic} and {this.props.subtopic}</h1>
+        const {topic,subtopic} = this.props
+        // const {state1,state2} = this.state
+        return <h1>Class Component of {topic} and {subtopic}</h1>
         // console.log(this.props);
         
     }
